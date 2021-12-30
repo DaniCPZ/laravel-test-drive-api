ed77e55033b0e22e426095c5b751dc750d14fdf2
Daniel Valenzuela <danielvalenzuela199825@gmail.com>
2021-12-30 19:12:31 -0300

    Add middleware

--- routes/api.php
+++ routes/api.php
@@ -6,11 +6,13 @@ use App\Http\Controllers\TodoListController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 
-Route::apiResource('todo-list', TodoListController::class);
+Route::middleware('auth:sanctum')->group(function(){
+    Route::apiResource('todo-list', TodoListController::class);
 
-Route::apiResource('todo-list.tasks', TaskController::class)
-    ->except('show')
-    ->shallow();
+    Route::apiResource('todo-list.tasks', TaskController::class)
+        ->except('show')
+        ->shallow();
+});
 
 Route::post('/register', RegisterController::class)->name('user.register');
 
